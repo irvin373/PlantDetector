43b302bc97edead3ffe5097c581f9aa866c7de7b
irvin373 <irvin373@gmail.com>
2019-09-09 21:49:02 -0400

    read plants from plants json

--- app/src/main/java/org/tensorflow/lite/examples/classification/plants/plantList.kt
+++ app/src/main/java/org/tensorflow/lite/examples/classification/plants/plantList.kt
@@ -1,6 +1,5 @@
 package org.tensorflow.lite.examples.classification.plants
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import org.tensorflow.lite.examples.classification.R
 
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_plant_list.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.InputStream
 
-class Attraction(private  val label: String) {
-}
 
 class plantList : Fragment() {
     val plants: ArrayList<Plant> = ArrayList()
@@ -25,10 +23,6 @@ class plantList : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_plant_list, container, false)
-//        recyclerview.apply {
-//            layoutManager = LinearLayoutManager(activity)
-//            adapter = PlantsAdapter(plants)
-//        }
         return view
     }
 
@@ -40,7 +34,24 @@ class plantList : Fragment() {
     }
 
     fun fillPlants() {
-        plants.add(Plant("Plnata 1", "Cardiaca", "its a description to display more and more to send a file with more la la la"))
-        plants.add(Plant("Planta 2", "Cardiaca", "its a description to display more and more to send a file with more la la la"))
+        var platsJson = JSONArray(readJSONFromAsset())
+        val size: Int = platsJson.length() -1
+        for (index in 0..size) {
+            val obj = JSONObject(platsJson[index].toString())
+            val plant = Plant(obj.optString("name"), obj.optString("group"), obj.optString("description"))
+            plants.add(plant)
+        }
+    }
+
+    fun readJSONFromAsset(): String? {
+        var json: String? = null
+        try {
+            val inputStream: InputStream = context!!.assets.open("plants.json")
+            json = inputStream.bufferedReader().use{it.readText()}
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
     }
 }
